f645ec5ede352b6fe336d08f69b557d8e56f4c04
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-07-23 00:34:40 +0530

    Add files via upload

---
+++ enum_example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+int main()  
+{  
+ enum day{Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};  
+printf("\n%d", Sunday);  
+printf("\n%d", Monday);  
+printf("\n%d", Tuesday);  
+printf("\n%d", Wednesday);  
+printf("\n%d", Thursday);  
+printf("\n%d", Friday);  
+printf("\n%d", Saturday);  
+
+    return 0;  
+} 
